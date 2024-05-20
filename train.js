ed1af79d1a81a8a6f901dbaf18552138bd102b2b
Sam <kimjeyeon03@icloud.com>
2024-05-20 21:37:34 +0900

    feat: develop train file

--- train.js
+++ train.js
@@ -1,7 +1,37 @@
-console.log("TRAIN AREA!");
-
-// B - TASK
-
-
-
+// console.log("TRAIN AREA!");
 // A - TASK
+
+// const setTimeout = require("setTimeOut");
+console.log("Jack Ma maslahatlari!");
+
+const list = [
+    "Yaxshi talaba bo'ling!",  // 0-20
+    "To'gri boshliq tanlang va Ko'proq xaton qiling!",  // 20-30
+    "O'zingizga ishlashni boshlang!",  // 30-40
+    "Siz kuchli bo'lgan narslarni qiling",  // 40-50
+    "Yoshlarga investitsiya qiling",  // 50-60
+    "Endi dam oling, Foydasi yo'q"  // 60
+];
+
+function maslahatBering(a, callback) {
+    if (typeof a !== "number") callback("insert a number", null);
+    else if (a <= 20) callback(null, list[0]);
+    else if (a > 20 && a <= 30) callback(null, list[1]);
+    else if (a > 30 && a <= 40) callback(null, list[2]);
+    else if (a > 40 && a <= 50) callback(null, list[3]);
+    else if (a > 50 && a <= 60) callback(null, list[4]);
+    else {
+        setTimeout(function () {
+            callback(null, list[5]);
+        }, 4000);
+    }
+};
+
+console.log("passed here 0");
+maslahatBering(65, (err, data) => {
+    if (err) console.log("ERROR:", err);
+    else {
+        console.log("javob:", data);
+    }
+});
+console.log("passed here 1");
