61e5d53961c3eea7fca5944dc2ffc49f3902e53c
Sam <kimjeyeon03@icloud.com>
2024-06-18 02:44:42 +0900

    fix: file

--- train.js
+++ train.js
@@ -1,5 +1,26 @@
 // console.log("TRAIN AREA!");
 
+// function engKopi(a: number[]): number | undefined {
+//     const b: { [key: number]: number } = {};
+  
+//     for (const num of a) {
+//       b[num] = (b[num] || 0) + 1;
+//     }
+  
+//     let c = 0;
+//     let eng: number | undefined;
+  
+//     for (const key in b) {
+//       if (b[key] > c) {
+//         c = b[key];
+//         eng = Number(key);
+//       }
+//     }
+  
+//     return eng;
+// }
+// console.log(engKopi([1, 2, 3, 0, 4, 5, 0, 6, 0, 7, 8, 9, 9, 9, 9, 9]));
+
 // // H2 - TASK
 
 // let b: string;
